89a99b4e56cf197962971ac80dc369e67aede35d
Aletropy <70422727+Aletropy@users.noreply.github.com>
2020-12-31 19:41:49 -0300

    update inter.js

--- scripts/inter.js
+++ scripts/inter.js
@@ -1,3 +1,31 @@
+//////////////////////////////////////////////////
+/////////////////// Variaveis ////////////////////
+//////////////////////////////////////////////////
+//Money
+var pugF = 0;
+var pugN = 0;
+var pugA = 0;
+var multClick = 1;
+//Geral
+var Unlock_pugN = false;
+var Unlock_pugA = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////
+/////////////////// Intervalos ///////////////////
+//////////////////////////////////////////////////
+
 setInterval(function() {
 if (Unlock_pugN === false && Unlock_pugA === false) {
 	document.getElementById("displayPug").innerHTML = "Pug Filhote: " + pugF;
@@ -8,3 +36,35 @@ if (Unlock_pugN === false && Unlock_pugA === false) {
 document.getElementById("displayPug").innerHTML = "Pug Filhote: " + pugF + "<br>Pug Novato: " + pugN + "<br>Pug Adulto: " + pugA;	
 }	
 }, 1)
+
+//////////////////////////////////////////////////
+/////////////////// Salvamento ///////////////////
+//////////////////////////////////////////////////
+
+
+function loadGame() {
+var dataSalva = JSON.parse(localStorage.getItem("data"));
+
+	if (typeof dataSalva.pugF !== "undefined") pugF = dataSalva.pugF;
+	if (typeof dataSalva.pugN !== "undefined") pugN = dataSalva.pugN;
+	if (typeof dataSalva.pugA !== "undefined") pugA = dataSalva.pugA;
+	if (typeof dataSalva.multClick !== "undefined") multClick = dataSalva.multClick;
+}
+
+function saveGame() {
+var data = {
+	pugF: pugF,
+	pugN: pugN,
+	pugA: pugA,
+	multClick: multClick
+ };
+ localStorage.setItem("data", JSON.stringify(data));
+}
+
+setInterval (function() {
+ saveGame();
+}, 1);
+
+window.onload = function() {
+	loadGame();
+};
